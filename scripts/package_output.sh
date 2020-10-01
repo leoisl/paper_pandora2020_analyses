57292c92277e465a14358c78b9bd9c65e9cbda5d
leandro <leandro@ebi.ac.uk>
2020-10-01 12:56:47 +0100

    ln -> cp just to avoid undesirable change/remove issues

--- scripts/package_output.sh
+++ scripts/package_output.sh
@@ -15,8 +15,8 @@ mkdir -p "${output_dir}"
 
 # pangenome variations
 mkdir -p "${output_dir}/technology_independent_analysis"
-ln -s "${pipeline_output}/pangenome_variations/output_pangenome_variations_pandora_paper_tag1/plot_pangenome_variants_vs_samples" "${output_dir}/technology_independent_analysis/pangenome_variants_vs_samples"
-ln -s "${pipeline_output}/pangenome_variations/output_pangenome_variations_pandora_paper_tag1/two_SNP_heatmap"                    "${output_dir}/technology_independent_analysis/two_SNP_heatmap"
+cp -vr "${pipeline_output}/pangenome_variations/output_pangenome_variations_pandora_paper_tag1/plot_pangenome_variants_vs_samples" "${output_dir}/technology_independent_analysis/pangenome_variants_vs_samples"
+cp -vr "${pipeline_output}/pangenome_variations/output_pangenome_variations_pandora_paper_tag1/two_SNP_heatmap"                    "${output_dir}/technology_independent_analysis/two_SNP_heatmap"
 
 
 # illumina/nanopore analysis
@@ -27,25 +27,25 @@ for technology in "${technologies[@]}"; do
     mkdir -p "${output_dir}/${technology}_analysis${filter}"
 
     # pandora 1 paper
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/enrichment_of_FPs"                          "${output_dir}/${technology}_analysis${filter}/enrichment_of_FPs"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/precision_per_ref_per_clade"                "${output_dir}/${technology}_analysis${filter}/precision_per_ref_per_clade"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/precision_per_sample"                       "${output_dir}/${technology}_analysis${filter}/precision_per_sample"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_nb_of_samples"                   "${output_dir}/${technology}_analysis${filter}/recall_per_nb_of_samples"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_ref_per_clade"                   "${output_dir}/${technology}_analysis${filter}/recall_per_ref_per_clade"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_ref_per_nb_of_samples_per_clade" "${output_dir}/${technology}_analysis${filter}/recall_per_ref_per_nb_of_samples_per_clade"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_sample"                          "${output_dir}/${technology}_analysis${filter}/recall_per_sample"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_sample_per_number_of_samples"    "${output_dir}/${technology}_analysis${filter}/recall_per_sample_per_number_of_samples"
-    ln -s "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/ROC_data.tsv"                               "${output_dir}/${technology}_analysis${filter}/ROC_data.tsv"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/enrichment_of_FPs"                          "${output_dir}/${technology}_analysis${filter}/enrichment_of_FPs"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/precision_per_ref_per_clade"                "${output_dir}/${technology}_analysis${filter}/precision_per_ref_per_clade"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/precision_per_sample"                       "${output_dir}/${technology}_analysis${filter}/precision_per_sample"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_nb_of_samples"                   "${output_dir}/${technology}_analysis${filter}/recall_per_nb_of_samples"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_ref_per_clade"                   "${output_dir}/${technology}_analysis${filter}/recall_per_ref_per_clade"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_ref_per_nb_of_samples_per_clade" "${output_dir}/${technology}_analysis${filter}/recall_per_ref_per_nb_of_samples_per_clade"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_sample"                          "${output_dir}/${technology}_analysis${filter}/recall_per_sample"
+    cp -vr "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/recall_per_sample_per_number_of_samples"    "${output_dir}/${technology}_analysis${filter}/recall_per_sample_per_number_of_samples"
+    cp -v "${pipeline_output}/pandora1_paper/analysis_output_${technology}_pandora_paper_tag1${filter}/plot_data/ROC_data.tsv"                               "${output_dir}/${technology}_analysis${filter}/ROC_data.tsv"
 
     # pandora_gene_distance
     if [ "$filter" = "" ]
     then
       mkdir -p "${output_dir}/${technology}_analysis/FP_genes"
-      ln -s "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/FP_genes/pandora_${technology}_100x_nodenovo"   "${output_dir}/${technology}_analysis/FP_genes/pandora_${technology}_100x_nodenovo"
-      ln -s "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/FP_genes/pandora_${technology}_100x_withdenovo" "${output_dir}/${technology}_analysis/FP_genes/pandora_${technology}_100x_withdenovo"
+      cp -vr "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/FP_genes/pandora_${technology}_100x_nodenovo"   "${output_dir}/${technology}_analysis/FP_genes/pandora_${technology}_100x_nodenovo"
+      cp -vr "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/FP_genes/pandora_${technology}_100x_withdenovo" "${output_dir}/${technology}_analysis/FP_genes/pandora_${technology}_100x_withdenovo"
       mkdir -p "${output_dir}/${technology}_analysis/gene_distance_plots"
-      ln -s "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/gene_distance_plots/pandora_${technology}_100x_nodenovo"   "${output_dir}/${technology}_analysis/gene_distance_plots/pandora_${technology}_100x_nodenovo"
-      ln -s "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/gene_distance_plots/pandora_${technology}_100x_withdenovo" "${output_dir}/${technology}_analysis/gene_distance_plots/pandora_${technology}_100x_withdenovo"
+      cp -vr "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/gene_distance_plots/pandora_${technology}_100x_nodenovo"   "${output_dir}/${technology}_analysis/gene_distance_plots/pandora_${technology}_100x_nodenovo"
+      cp -vr "${pipeline_output}/pandora_gene_distance/gene_distance_pandora_paper_tag1/gene_distance_plots/pandora_${technology}_100x_withdenovo" "${output_dir}/${technology}_analysis/gene_distance_plots/pandora_${technology}_100x_withdenovo"
     fi
   done
 done
