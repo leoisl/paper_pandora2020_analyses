009fbca9f6732ac268e284d710ffe7c74e8d77ca
leandro <leandro@ebi.ac.uk>
2020-10-06 12:00:23 +0100

    Fixing Figure6.R

--- scripts/csv_to_paper_plots/Figure6/Figure6.R
+++ scripts/csv_to_paper_plots/Figure6/Figure6.R
@@ -2,21 +2,21 @@ library(png)
 library(grid)
 library(gridExtra)
 
-png(file="Figure6.png", width=1400, height=1100)
+png(file="Figure6.png", width=1400, height=900)
 
 illumina_20_way <- grid::rasterGrob(readPNG("../20wayROC/ROC_data_20_way_illumina.R_data.png"))
 nanopore_20_way <- grid::rasterGrob(readPNG("../20wayROC/ROC_data_20_way_nanopore.R_data.png"))
-precision_per_sample <- grid::rasterGrob(readPNG("../precision_per_sample/precision_per_sample.png"))
+precision_per_sample_illumina <- grid::rasterGrob(readPNG("../precision_per_sample/precision_per_sample_illumina.png"))
+precision_per_sample_nanopore <- grid::rasterGrob(readPNG("../precision_per_sample/precision_per_sample_nanopore.png"))
 
 
-layout <- rbind(c(1,2),
-                c(3,3))
 gridExtra::grid.arrange(
   arrangeGrob(illumina_20_way, top = textGrob("A",  x = 0.05, gp=gpar(fontsize=30))),
   arrangeGrob(nanopore_20_way, top = textGrob("B",  x = 0.05, gp=gpar(fontsize=30))),
-  arrangeGrob(precision_per_sample, top = textGrob("C",  x = 0.05, gp=gpar(fontsize=30))),
-  layout_matrix=layout,
-  heights= c(500, 500),
+  arrangeGrob(precision_per_sample_illumina, top = textGrob("C",  x = 0.05, gp=gpar(fontsize=30))),
+  arrangeGrob(precision_per_sample_nanopore, top = textGrob("D",  x = 0.05, gp=gpar(fontsize=30))),
+  ncol=2,
+  heights= c(500, 400),
   widths = c(700, 700)
   )
 
