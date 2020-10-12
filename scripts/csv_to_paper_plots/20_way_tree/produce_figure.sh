693cc18641b1113d29b1aa6cc9ba412f07cebe61
leandro <leandro@ebi.ac.uk>
2020-10-12 11:09:19 +0100

    Adding 20_way_tree/produce_figure.sh

---
+++ scripts/csv_to_paper_plots/20_way_tree/produce_figure.sh
@@ -0,0 +1,6 @@
+# no figure is produced here, as it is not done through a script
+
+# there is a preprocessing done:
+#   1. see produce_20_way_tree_fig.sh script;
+#   2. input data can be found at https://github.com/iqbal-lab/pandora1_paper/issues/203
+#   3. plotting is done with iTol (link to be added)
