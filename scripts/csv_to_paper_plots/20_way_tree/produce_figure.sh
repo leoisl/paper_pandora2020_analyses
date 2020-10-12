c9634abeee36cbd87eacb86e066f4127a6d58524
leandro <leandro@ebi.ac.uk>
2020-10-12 11:28:24 +0100

    Adding iTol link, closes #8

--- scripts/csv_to_paper_plots/20_way_tree/produce_figure.sh
+++ scripts/csv_to_paper_plots/20_way_tree/produce_figure.sh
@@ -3,4 +3,4 @@
 # there is a preprocessing done:
 #   1. see produce_20_way_tree_fig.sh script;
 #   2. input data can be found at https://github.com/iqbal-lab/pandora1_paper/issues/203
-#   3. plotting is done with iTol (link to be added)
+#   3. plotting is done with iTol, see https://itol.embl.de/tree/1722225420362651601369875#
