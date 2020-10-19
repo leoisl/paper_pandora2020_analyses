bdc242ffb5057dd0a5c36f64e0b5e0a5ae25d79c
leandro <leandro@ebi.ac.uk>
2020-10-19 18:59:34 +0100

    Adding Supplementary Figure A10

--- scripts/csv_to_paper_plots/recall_per_ref_per_clade_supplementary/preprocess.py
+++ scripts/csv_to_paper_plots/recall_per_ref_per_clade_supplementary/preprocess.py
@@ -15,3 +15,7 @@ df.to_csv("recall_per_ref_per_clade_medaka_pandora.simplified.csv", index=False)
 df = pd.read_csv("../pandora1_paper_analysis_output_20_way/nanopore_analysis/recall_per_ref_per_clade/recall_per_ref_per_clade_nanopolish_pandora.csv")
 df["ref"] = df["ref"].apply(lambda ref: ref.split()[0])
 df.to_csv("recall_per_ref_per_clade_nanopolish_pandora.simplified.csv", index=False)
+
+df = pd.read_csv("../pandora1_paper_analysis_output_20_way/illumina_analysis/recall_per_ref_per_nb_of_samples_per_clade/recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.csv")
+df["ref"] = df["ref"].apply(lambda ref: ref.split()[0])
+df.to_csv("recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.simplified.csv", index=False)
