a7fb9db645ee6b34b592f4ccc8f07f053555a2b6
leandro <leandro@ebi.ac.uk>
2020-10-19 18:58:26 +0100

    Small bugfix on Figure 8

--- scripts/csv_to_paper_plots/Figure8/preprocess.py
+++ scripts/csv_to_paper_plots/Figure8/preprocess.py
@@ -6,4 +6,4 @@ df.to_csv("recall_per_ref_per_clade_snippy_pandora.simplified.csv", index=False)
 
 df = pd.read_csv("../pandora1_paper_analysis_output_20_way/illumina_analysis/recall_per_ref_per_nb_of_samples_per_clade/recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.csv")
 df["ref"] = df["ref"].apply(lambda ref: ref.split()[0])
-df.to_csv("recall_per_ref_per_nb_of_samples_per_clade.samtools_pandora.nb_of_samples_2.simplified.csv", index=False)
+df.to_csv("recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.simplified.csv", index=False)
