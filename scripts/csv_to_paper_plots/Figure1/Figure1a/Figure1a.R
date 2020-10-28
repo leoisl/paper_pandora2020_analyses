e4fb6a7b6a2fe039df218b8349fbd6a2bdafc3eb
leandro <leandro@ebi.ac.uk>
2020-10-28 17:18:50 +0000

    Updating Figure1a with Leah changes

--- scripts/csv_to_paper_plots/Figure1/Figure1a/Figure1a.R
+++ scripts/csv_to_paper_plots/Figure1/Figure1a/Figure1a.R
@@ -27,23 +27,52 @@
 #*Plot in R:*
 
 library("ggplot2")
+library(ggthemr)
 
+ggthemr('fresh')
+# Set linetype legend colour
+update_geom_defaults("line",   list(colour = "grey"))
 
 data <- read.csv("roary_output/all_count.txt", sep='\t', header=TRUE)
 
 count_plot <- ggplot(data, aes(x=count, fill=species)) + geom_bar(position="dodge") +
   scale_x_discrete(limits=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")) + 
-  theme(axis.title.x = element_blank(), axis.title.y = element_blank())
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size=12)) +
+  scale_fill_discrete(
+    "Species",
+    labels = c(expression(italic("E. coli")),
+               expression(italic("K. pneumoniae")),
+               expression(italic("P. aeruginosa")),
+               expression(italic("S. aureus")),
+               expression(italic("S. enterica")),
+               expression(italic("S. pneumoniae")))) +
+  theme(legend.text.align = 0)
+
 
 ggsave(count_plot, file="Figure1a.count.png", width=7, height=5, dpi=300)
 
 
 data <- read.csv("roary_output/gene_props_all.tsv", sep='\t', header=TRUE)
 
-proportion_plot <- ggplot(data, aes(x=genomes, y=prop, group=species)) + geom_line(aes(linetype=species, color=species)) +
-  geom_point(aes(color=species)) +
+proportion_plot <- ggplot(data, aes(x=genomes, y=prop, fill=species)) + geom_bar(stat='identity', position="dodge") +
   scale_x_discrete(limits=c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")) +
-  theme(axis.title.x = element_blank(), axis.title.y = element_blank())
+  scale_y_continuous(breaks = c(0.1, 0.2, 0.3, 0.4, 0.5)) +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_text(size=12),
+        axis.text.y = element_text(size=12)) +
+  scale_fill_discrete(
+    "Species",
+    labels = c(expression(italic("E. coli")),
+               expression(italic("K. pneumoniae")),
+               expression(italic("P. aeruginosa")),
+               expression(italic("S. aureus")),
+               expression(italic("S. enterica")),
+               expression(italic("S. pneumoniae")))) +
+  theme(legend.text.align = 0)
 
 
 ggsave(proportion_plot, file="Figure1a.proportion.png", width=7, height=5, dpi=300)
