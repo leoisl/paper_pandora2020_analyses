c9df2dde7f3c6984669907dc9863c06cdbf6520f
leandro <leandro@ebi.ac.uk>
2020-11-02 21:50:47 +0000

    Updating supplementary figures numbering

--- scripts/csv_to_paper_plots/generate_paper_plots_core.sh
+++ scripts/csv_to_paper_plots/generate_paper_plots_core.sh
@@ -40,13 +40,13 @@ cp 4wayROC/ROC_data_old_and_new_basecall.png paper_plots/Figure5.png
 echo "Generating Figure 6..."
 cd Figure6 && bash produce_figure.sh && cd ..
 cp Figure6/Figure6.png paper_plots/Figure6.png
-cp Figure6/Figure6_supplementary.png paper_plots/SupplementaryFigureA2.png
-cp Figure6/ROC_with_PVR_Supplementary.png paper_plots/SupplementaryFigureA3.png
+cp Figure6/Figure6_supplementary.png paper_plots/SupplementaryFigureA1.png
+cp Figure6/ROC_with_PVR_Supplementary.png paper_plots/SupplementaryFigureA2.png
 
 echo "Generating Figure 7..."
 cd recall_per_nb_of_samples && bash produce_figure.sh && cd ..
 cp recall_per_nb_of_samples/leandro/Figure7.png paper_plots/Figure7.png
-cp recall_per_nb_of_samples/leandro/SupplementaryFigureA5.png paper_plots/SupplementaryFigureA5.png
+cp recall_per_nb_of_samples/leandro/SupplementaryFigureA4.png paper_plots/SupplementaryFigureA4.png
 
 echo "Generating Figure 8..."
 cd Figure8 && bash produce_figure.sh && cd ..
@@ -60,17 +60,17 @@ echo "Generating Figure 10..."
 cd Figure10 && bash produce_figure.sh && cd ..
 cp Figure10/Figure10.sample_gene_pairs_within_1perc_distance_gene_count_up_to_20.nanopore.png paper_plots/Figure10.png
 
-echo "Generating Supplementary Figure A.4"
+echo "Generating Supplementary Figure A.3"
 cd gene_classification && bash produce_figure.sh && cd ..
-cp gene_classification/gene_classification.png paper_plots/SupplementaryFigureA4.png
+cp gene_classification/gene_classification.png paper_plots/SupplementaryFigureA3.png
 
-echo "Generating Supplementary Figure A.6-9"
+echo "Generating Supplementary Figure A.5-9"
 cd recall_per_ref_per_clade_supplementary && bash produce_figure.sh && cd ..
-cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_snippy_pandora.png paper_plots/SupplementaryFigureA6.png
-cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_samtools_pandora.png paper_plots/SupplementaryFigureA7.png
-cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_medaka_pandora.png paper_plots/SupplementaryFigureA8.png
-cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_nanopolish_pandora.png paper_plots/SupplementaryFigureA9.png
-cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.png paper_plots/SupplementaryFigureA10.png
+cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_snippy_pandora.png paper_plots/SupplementaryFigureA5.png
+cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_samtools_pandora.png paper_plots/SupplementaryFigureA6.png
+cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_medaka_pandora.png paper_plots/SupplementaryFigureA7.png
+cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_clade_nanopolish_pandora.png paper_plots/SupplementaryFigureA8.png
+cp recall_per_ref_per_clade_supplementary/recall_per_ref_per_nb_of_samples_per_clade.snippy_pandora.nb_of_samples_2.png paper_plots/SupplementaryFigureA9.png
 
 echo "Generating Supplementary Animation 2"
 cd supplementary_animation_2 && bash produce_figure.sh && cd ..
