4c37c931ec382fbc8337be844b64d97cac2b7e7d
Leandro Ishi <leandro.ishi.lima@gmail.com>
2021-06-14 21:21:08 +0100

    Updating pull_containers.sh

--- scripts/pull_containers/pull_containers.sh
+++ scripts/pull_containers/pull_containers.sh
@@ -2,8 +2,8 @@
 set -eux
 
 # downloading containers
-mkdir -p paper_pandora2020_containers
-cd paper_pandora2020_containers
+mkdir -p containers
+cd containers
 
 singularity pull docker://leandroishilima/varifier:pandora_paper_tag1
 singularity pull docker://leandroishilima/subsampler:pandora_paper_tag1
@@ -18,7 +18,12 @@ singularity pull docker://leandroishilima/pandora_gene_distance_indexing_mapping
 singularity pull docker://leandroishilima/pandora1_paper_full_pipeline_r_container:pandora_paper_tag1
 
 # renaming to the repo names used in the publication
-mv pandora_analysis_pipeline-pandora_paper_tag1.simg pandora_workflow-pandora_paper_tag1.simg
-mv pandora1_paper_basic_tools-pandora_paper_tag1.simg pandora_roc_basic_tools-pandora_paper_tag1.simg
-mv pandora1_paper_r-pandora_paper_tag1.simg pandora_roc_r-pandora_paper_tag1.simg
-mv pandora1_paper_full_pipeline_r_container-pandora_paper_tag1.simg paper_pandora2020_analyses_r_container-pandora_paper_tag1.simg
+mv varifier-pandora_paper_tag1.simg varifier.simg
+mv subsampler-pandora_paper_tag1.simg subsampler.simg
+mv pandora_analysis_pipeline-pandora_paper_tag1.simg pandora_workflow.simg
+mv variant_callers_pipeline_samtools-pandora_paper_tag1.simg samtools.simg
+mv variant_callers_pipeline_nanopolish-pandora_paper_tag1.simg nanopolish.simg
+mv pandora1_paper_basic_tools-pandora_paper_tag1.simg pandora_roc_basic_tools.simg
+mv pandora1_paper_r-pandora_paper_tag1.simg pandora_roc_r.simg
+mv pandora_gene_distance_indexing_mapping-pandora_paper_tag1.simg pandora_gene_distance_indexing_mapping.simg
+mv pandora1_paper_full_pipeline_r_container-pandora_paper_tag1.simg paper_pandora2020_analyses_r_container.simg
