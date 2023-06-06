dedfd00c7a7f313b32bd8639383913fbea680947
Leandro Ishi <leandro.ishi.lima@gmail.com>
2023-06-06 16:00:15 +0100

    removing unnecessary pip install

--- scripts/run_4_way.sh
+++ scripts/run_4_way.sh
@@ -22,7 +22,6 @@ if ! test -f "${flag_file}"; then
   echo "Running installation pipeline..."
   cd installation_pipeline
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --restart-times 0 --profile "$profile" --config input_data_folder="$input_data_folder" \
     pipeline_output="$pipeline_output" || { echo 'FATAL ERROR: installation pipeline failed;' ; exit 1; }
   deactivate
@@ -39,7 +38,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pangenome_variations pipeline..."
   cd ${pipeline_output}/pangenome_variations
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --profile "$profile" --configfile config.pandora_paper_tag1.4way_old_basecall.yaml --singularity-prefix "${singularity_prefix}" \
     || { echo 'FATAL ERROR: pangenome_variations pipeline old basecall failed;' ; exit 1; }
   snakemake --profile "$profile" --configfile config.pandora_paper_tag1.4way_new_basecall.yaml --singularity-prefix "${singularity_prefix}" \
@@ -58,7 +56,6 @@ if ! test -f "${flag_file}"; then
   echo "Running subsampler pipeline..."
   cd ${pipeline_output}/subsampler
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --profile "$profile" --configfile config.pandora_paper_tag1.4way.yaml --singularity-prefix "${singularity_prefix}" \
     || { echo 'FATAL ERROR: subsampler pipeline for nanopore failed;' ; exit 1; }
   snakemake --profile "$profile" --configfile config.pandora_paper_tag1.4way.illumina.yaml --singularity-prefix "${singularity_prefix}" \
@@ -77,7 +74,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_workflow old basecall..."
   cd ${pipeline_output}/pandora_workflow
   source venv/bin/activate
-  pip install snakemake==7.26.0
   bash scripts/run_pipeline_lsf.sh --configfile config.pandora_paper_tag1.4_way_old_basecall.yaml --singularity-prefix "${singularity_prefix}" \
     || { echo 'FATAL ERROR: pandora_workflow old basecall failed;' ; exit 1; }
   deactivate
@@ -93,7 +89,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_workflow new basecall..."
   cd ${pipeline_output}/pandora_workflow
   source venv/bin/activate
-  pip install snakemake==7.26.0
   bash scripts/run_pipeline_lsf.sh --configfile config.pandora_paper_tag1.4_way_new_basecall.yaml --singularity-prefix "${singularity_prefix}" \
     || { echo 'FATAL ERROR: pandora_workflow new basecall failed;' ; exit 1; }
   deactivate
@@ -109,7 +104,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_workflow illumina..."
   cd ${pipeline_output}/pandora_workflow
   source venv/bin/activate
-  pip install snakemake==7.26.0
   bash scripts/run_pipeline_lsf.sh --configfile config.pandora_paper_tag1.4_way_illumina.yaml --singularity-prefix "${singularity_prefix}" \
     || { echo 'FATAL ERROR: pandora_workflow illumina failed;' ; exit 1; }
   deactivate
@@ -125,7 +119,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_paper_roc pipeline old basecall..."
   cd ${pipeline_output}/pandora_paper_roc
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --local-cores "$LOCAL_CORES" --profile "$profile" \
     --groups calculate_precision_per_sample_no_gt_conf=group_1 calculate_recall_per_sample_no_gt_conf_filter=group_2 calculate_recall_per_sample_vs_nb_of_samples=group_3 create_precision_report_from_probe_mappings=group_4 create_recall_report_for_truth_variants_mappings=group_5 create_recall_report_per_sample_for_calculator=group_6 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_medaka=group_7 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_nanopolish=group_8 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_samtools=group_9 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy=group_10 fix_medaka_vcf_for_pipeline=group_11 fix_nanopolish_vcf_for_pipeline=group_12 fix_samtools_vcf_for_pipeline=group_13 fix_snippy_vcf_for_pipeline=group_14 make_mutated_vcf_ref_for_recall=group_15 make_variant_calls_probeset_for_precision=group_16 make_vcf_for_a_single_sample=group_17 map_recall_truth_probeset_to_mutated_vcf_ref=group_18 map_variant_call_probeset_to_reference_assembly=group_19 \
     --group-components group_1=10 group_2=10 group_3=10 group_4=10 group_5=10 group_6=5 group_7=10 group_8=10 group_9=10 group_10=10 group_11=10 group_12=10 group_13=10 group_14=10 group_15=10 group_16=10 group_17=10 group_18=10 group_19=10 \
@@ -144,7 +137,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_paper_roc pipeline new basecall..."
   cd ${pipeline_output}/pandora_paper_roc
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --local-cores "$LOCAL_CORES" --profile "$profile" \
     --groups calculate_precision_per_sample_no_gt_conf=group_1 calculate_recall_per_sample_no_gt_conf_filter=group_2 calculate_recall_per_sample_vs_nb_of_samples=group_3 create_precision_report_from_probe_mappings=group_4 create_recall_report_for_truth_variants_mappings=group_5 create_recall_report_per_sample_for_calculator=group_6 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_medaka=group_7 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_nanopolish=group_8 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_samtools=group_9 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy=group_10 fix_medaka_vcf_for_pipeline=group_11 fix_nanopolish_vcf_for_pipeline=group_12 fix_samtools_vcf_for_pipeline=group_13 fix_snippy_vcf_for_pipeline=group_14 make_mutated_vcf_ref_for_recall=group_15 make_variant_calls_probeset_for_precision=group_16 make_vcf_for_a_single_sample=group_17 map_recall_truth_probeset_to_mutated_vcf_ref=group_18 map_variant_call_probeset_to_reference_assembly=group_19 \
     --group-components group_1=10 group_2=10 group_3=10 group_4=10 group_5=10 group_6=5 group_7=10 group_8=10 group_9=10 group_10=10 group_11=10 group_12=10 group_13=10 group_14=10 group_15=10 group_16=10 group_17=10 group_18=10 group_19=10 \
@@ -163,7 +155,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_paper_roc pipeline with filters..."
   cd ${pipeline_output}/pandora_paper_roc
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --local-cores "$LOCAL_CORES" --profile "$profile" \
     --groups calculate_precision_per_sample_no_gt_conf=group_1 calculate_recall_per_sample_no_gt_conf_filter=group_2 calculate_recall_per_sample_vs_nb_of_samples=group_3 create_precision_report_from_probe_mappings=group_4 create_recall_report_for_truth_variants_mappings=group_5 create_recall_report_per_sample_for_calculator=group_6 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_medaka=group_7 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_nanopolish=group_8 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_samtools=group_9 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy=group_10 fix_medaka_vcf_for_pipeline=group_11 fix_nanopolish_vcf_for_pipeline=group_12 fix_samtools_vcf_for_pipeline=group_13 fix_snippy_vcf_for_pipeline=group_14 make_mutated_vcf_ref_for_recall=group_15 make_variant_calls_probeset_for_precision=group_16 make_vcf_for_a_single_sample=group_17 map_recall_truth_probeset_to_mutated_vcf_ref=group_18 map_variant_call_probeset_to_reference_assembly=group_19 \
     --group-components group_1=10 group_2=10 group_3=10 group_4=10 group_5=10 group_6=5 group_7=10 group_8=10 group_9=10 group_10=10 group_11=10 group_12=10 group_13=10 group_14=10 group_15=10 group_16=10 group_17=10 group_18=10 group_19=10 \
@@ -182,7 +173,6 @@ if ! test -f "${flag_file}"; then
   echo "Running pandora_paper_roc pipeline with filters for illumina..."
   cd ${pipeline_output}/pandora_paper_roc
   source venv/bin/activate
-  pip install snakemake==7.26.0
   snakemake --local-cores "$LOCAL_CORES" --profile "$profile" \
     --groups calculate_precision_per_sample_no_gt_conf=group_1 calculate_recall_per_sample_no_gt_conf_filter=group_2 calculate_recall_per_sample_vs_nb_of_samples=group_3 create_precision_report_from_probe_mappings=group_4 create_recall_report_for_truth_variants_mappings=group_5 create_recall_report_per_sample_for_calculator=group_6 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_medaka=group_7 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_nanopolish=group_8 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_samtools=group_9 filter_vcf_for_a_single_sample_by_gt_conf_percentile_for_snippy=group_10 fix_medaka_vcf_for_pipeline=group_11 fix_nanopolish_vcf_for_pipeline=group_12 fix_samtools_vcf_for_pipeline=group_13 fix_snippy_vcf_for_pipeline=group_14 make_mutated_vcf_ref_for_recall=group_15 make_variant_calls_probeset_for_precision=group_16 make_vcf_for_a_single_sample=group_17 map_recall_truth_probeset_to_mutated_vcf_ref=group_18 map_variant_call_probeset_to_reference_assembly=group_19 \
     --group-components group_1=10 group_2=10 group_3=10 group_4=10 group_5=10 group_6=5 group_7=10 group_8=10 group_9=10 group_10=10 group_11=10 group_12=10 group_13=10 group_14=10 group_15=10 group_16=10 group_17=10 group_18=10 group_19=10 \
